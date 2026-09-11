79b9f6e40974f6ea7497bea1060be1598ea91d15
eliasdds1974 <eliasdds@gmail.com>
2026-09-11 14:24:09 -0700

    Use DentalJobs Near You heading in both portals

--- app/dental-jobs/layout.tsx
+++ app/dental-jobs/layout.tsx
@@ -1,11 +1,13 @@
 import type { ReactNode } from "react";
 import { DoNotMatchPanel } from "@/components/DoNotMatchPanel";
 import { DentalJobsRoleSync } from "@/components/DentalJobsRoleSync";
+import { DentalJobsHeadingSync } from "@/components/DentalJobsHeadingSync";
 
 export default function DentalJobsLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <DentalJobsRoleSync />
+      <DentalJobsHeadingSync />
       <DoNotMatchPanel />
       {children}
     </>
