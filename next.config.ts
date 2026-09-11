a15ad6c86c2d68947cabf42173a9327daaa69a24
eliasdds1974 <eliasdds@gmail.com>
2026-09-11 11:26:11 -0700

    Redirect classifieds to dental-jobs

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,15 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: "/classifieds",
+        destination: "/dental-jobs",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
