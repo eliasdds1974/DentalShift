fe27cc9d276fcb1fed87f89d3bb9165dce8e15b4
eliasdds1974 <eliasdds@gmail.com>
2026-09-11 20:15:26 -0700

    Remove post-render DentalJobs marketplace modifiers

--- app/dental-jobs/layout.tsx
+++ app/dental-jobs/layout.tsx
@@ -1,21 +1,13 @@
 import type { ReactNode } from "react";
 import { DentalJobsDoNotMatchPlacement } from "@/components/DentalJobsDoNotMatchPlacement";
 import { DoNotMatchPdfDownload } from "@/components/DoNotMatchPdfDownload";
-import { DentalJobsRoleSync } from "@/components/DentalJobsRoleSync";
-import { DentalJobsHeadingSync } from "@/components/DentalJobsHeadingSync";
 import { DentalJobsCancelPostingPolish } from "@/components/DentalJobsCancelPostingPolish";
-import { DentalJobsDistanceSync } from "@/components/DentalJobsDistanceSync";
 import { DentalJobsDynamicFooter } from "@/components/DentalJobsDynamicFooter";
-import { DentalJobsPortalMarketplaceFilter } from "@/components/DentalJobsPortalMarketplaceFilter";
 
 export default function DentalJobsLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <DentalJobsRoleSync />
-      <DentalJobsHeadingSync />
       <DentalJobsCancelPostingPolish />
-      <DentalJobsDistanceSync />
-      <DentalJobsPortalMarketplaceFilter />
       <DentalJobsDoNotMatchPlacement />
       <DoNotMatchPdfDownload />
       {children}
