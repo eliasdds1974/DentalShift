2f6e4a955a5b93d320e5c69f893d80fd90e7485f
eliasdds1974 <eliasdds@gmail.com>
2026-09-13 13:00:43 -0700

    Make resilient account loader server safe

--- lib/dentalshift-safe.ts
+++ lib/dentalshift-safe.ts
@@ -8,7 +8,7 @@ function withTimeout<T>(promise: Promise<T>, ms: number, label: string): Promise
   return Promise.race([
     promise,
     new Promise<T>((_, reject) => {
-      window.setTimeout(() => reject(new Error(`${label} timed out.`)), ms);
+      setTimeout(() => reject(new Error(`${label} timed out.`)), ms);
     }),
   ]);
 }
