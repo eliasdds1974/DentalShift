ea0c097cab8c7cdda8a7530d913043c5c75ceb2c
eliasdds1974 <eliasdds@gmail.com>
2026-09-13 16:24:07 -0700

    Apply office-style layout to professional postings

--- app/dental-jobs/layout.tsx
+++ app/dental-jobs/layout.tsx
@@ -3,6 +3,7 @@ import { DentalJobsDoNotMatchPlacement } from "@/components/DentalJobsDoNotMatch
 import { DoNotMatchPdfDownload } from "@/components/DoNotMatchPdfDownload";
 import { DentalJobsCancelPostingPolish } from "@/components/DentalJobsCancelPostingPolish";
 import { DentalJobsDynamicFooter } from "@/components/DentalJobsDynamicFooter";
+import { ProfessionalPostingsCardParity } from "@/components/ProfessionalPostingsCardParity";
 
 export default function DentalJobsLayout({ children }: { children: ReactNode }) {
   return (
@@ -10,6 +11,7 @@ export default function DentalJobsLayout({ children }: { children: ReactNode }) 
       <DentalJobsCancelPostingPolish />
       <DentalJobsDoNotMatchPlacement />
       <DoNotMatchPdfDownload />
+      <ProfessionalPostingsCardParity />
       {children}
       <DentalJobsDynamicFooter />
     </>
